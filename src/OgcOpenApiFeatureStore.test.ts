db8f029ff15d8f9ef4643e93208f12364c0f8b92
felipecarrillo100 <felipecarrillo100@gmail.com>
2023-01-19 12:44:12 +0100

    Clean up + additional tests

--- src/OgcOpenApiFeatureStore.test.ts
+++ src/OgcOpenApiFeatureStore.test.ts
@@ -5,24 +5,49 @@ import "isomorphic-fetch";
 import {OgcOpenApiFeatureStore} from "./OgcOpenApiFeatureStore";
 import {GeoJsonCodec} from "@luciad/ria/model/codec/GeoJsonCodec";
 import {getReference} from "@luciad/ria/reference/ReferenceProvider";
+import {createBounds} from "@luciad/ria/shape/ShapeFactory";
+import {Cursor} from "@luciad/ria/model/Cursor";
 
 jest.useFakeTimers();
 
 
         describe('OgcOpenApiFeatureStore ',  () => {
-            const model = {"generateIDs":false,"outputFormat":"application/geo+json","dataUrl":"https://demo.pygeoapi.io/master/collections/lakes/items","extent":{"spatial":{"bbox":[[-180,-90,180,90]],"crs":"http://www.opengis.net/def/crs/OGC/1.3/CRS84"},"temporal":{"interval":[["2011-11-11T00:00:00+00:00",null]]}},"tmp_reference":"CRS:84","typeName":"lakes","versions":["3.0.2"],"methods":["POST"],"useProxy":false,"beforeProxy":"https://demo.pygeoapi.io/master/collections/lakes/items","credentials":false,"requestHeaders":{}}
+            const model = {
+                "generateIDs":false,"outputFormat":"application/geo+json",
+                "dataUrl":"https://demo.pygeoapi.io/master/collections/lakes/items",
+                "extent":{"spatial":{"bbox":[[-180,-90,180,90]],
+                        "crs":"http://www.opengis.net/def/crs/OGC/1.3/CRS84"},
+                    "temporal":{"interval":[["2011-11-11T00:00:00+00:00",null]]}},
+                "tmp_reference":"CRS:84",
+                "typeName":"lakes",
+                "versions":["3.0.2"],
+                "methods":["POST"],
+                "useProxy":false,
+                "beforeProxy":"https://demo.pygeoapi.io/master/collections/lakes/items",
+                "credentials":false,
+                "requestHeaders":{}
+            }
+
             const store = new OgcOpenApiFeatureStore({
                 dataUrl: model.dataUrl,
                 outputFormat: model.outputFormat,
-                dataFormat: model.outputFormat,
                 codec: new GeoJsonCodec({generateIDs: false}),
                 reference: getReference(model.tmp_reference),
-                tmp_reference: model.tmp_reference,
                 requestHeaders:{},
                 featureUrl: model.dataUrl,
                 useCrs84Bounds: true,
                 customCrs: model.tmp_reference,
-                extent: { spatial: { bbox: [] } },
+            })
+
+            const storeWindmills = new OgcOpenApiFeatureStore({
+                dataUrl: "https://demo.pygeoapi.io/master/collections/dutch_windmills/items",
+                outputFormat: model.outputFormat,
+                codec: new GeoJsonCodec({generateIDs: false}),
+                reference: getReference(model.tmp_reference),
+                requestHeaders:{},
+                featureUrl: model.dataUrl,
+                useCrs84Bounds: true,
+                customCrs: model.tmp_reference,
             })
 
             it('OgcOpenApiFeatureStore.get feature', async () => {
@@ -31,6 +56,40 @@ jest.useFakeTimers();
                     expect(feature.id).toBe(1);
                 }, (err) => {
                     expect(2).toBe(3);
+                })
+            });
+
+            it('OgcOpenApiFeatureStore.spatialQuery', async () => {
+                const query = {"filter":null,"limit":1000};
+                const bounds = createBounds(getReference(model.tmp_reference), [ 3.40, 0.6, 51.15, 0.15])
+
+                const promiseToCursor = storeWindmills.spatialQuery(bounds, query) as Promise<Cursor>;
+                return promiseToCursor.then(cursor => {
+                    let counter = 0
+                    while (cursor.hasNext()) {
+                        const feature = cursor.next();
+                        counter++;
+                    }
+                    expect(counter).toBe(5);
+                }, (err) => {
+                    expect(true).toBe(false);
+                })
+            });
+
+            it('OgcOpenApiFeatureStore.spatialQuery with limit 2', async () => {
+                const query = {"filter":null,"limit":2};
+                const bounds = createBounds(getReference(model.tmp_reference), [ 3.40, 0.6, 51.15, 0.15])
+
+                const promiseToCursor = storeWindmills.spatialQuery(bounds, query) as Promise<Cursor>;
+                return promiseToCursor.then(cursor => {
+                    let counter = 0
+                    while (cursor.hasNext()) {
+                        const feature = cursor.next();
+                        counter++;
+                    }
+                    expect(counter).toBe(2);
+                }, (err) => {
+                    expect(true).toBe(false);
                 })
             });
         })
